7c598c7e036e856e58413145d56b0203a38e6036
Junkai Lu <junkai-lu@outlook.com>
2017-08-11 03:02:42 +0800

    2017/8/11

--- src/minimax.hpp
+++ src/minimax.hpp
@@ -33,6 +33,126 @@ namespace gadt
 {
 	namespace minimax
 	{
+		//allow check warning if it is true.
+		constexpr const bool g_ENABLE_MINIMAX_WARNING = true;
 
+		//AgentIndex is the type index of each player, default is int8_t.
+		using AgentIndex = int8_t;
+		using EvalValue = double;
+
+		template<typename State, typename Action, bool _is_debug>
+		class MinimaxNode
+		{
+		public:
+			using pointer = MinimaxNode<State, Action, _is_debug>*;
+			using reference = MinimaxNode<State, Action, _is_debug>&;
+			using Node = MinimaxNode<State, Action, _is_debug>;
+			using ActionSet = std::vector<Action>;
+
+			struct ParamPackage
+			{
+			public:
+				using GetNewStateFunc = std::function<State(const State&, const Action&)>;
+				using MakeActionFunc = std::function<void(const State&, ActionSet&)>;
+				using DetemineWinnerFunc = std::function<AgentIndex(const State&)>;
+				using EvalForParentFunc = std::function<EvalValue(const State&, const AgentIndex)>;
+
+			public:
+				//necessary functions.
+				const AgentIndex			no_winner_index;	//the index of no winner
+				const GetNewStateFunc		GetNewState;		//get a new state from previous state and action.
+				const MakeActionFunc		MakeAction;			//the function which create action set by the state.
+				const DetemineWinnerFunc	DetemineWinner;		//return no_winner_index if a state is not terminal state.
+				const EvalForParentFunc		EvalForParent;		//get the eval for parent node.
+
+			public:
+				ParamPackage(
+					AgentIndex				_no_winner_index,
+					GetNewStateFunc			_GetNewState,
+					MakeActionFunc			_MakeAction,
+					DetemineWinnerFunc		_DetemineWinner,
+					EvalForParentFunc		_EvalForParent
+				) :
+					no_winner_index(_no_winner_index),
+					GetNewState(_GetNewState),
+					MakeAction(_MakeAction),
+					DetemineWinner(_DetemineWinner),
+					EvalForParent(_EvalForParent)
+				{
+				}
+			};
+
+		private:
+			const State			_state;		//game state
+			const size_t		_depth;		//depth of the node
+			ActionSet			_actions;	//action set
+			AgentIndex			_winner;	//winner of the node.
+			const ParamPackage& _params;	//parameters package
+
+		private:
+			inline NodeInit()
+			{
+				_winner = _params.DetemineWinner(_state);
+				_params.MakeAction(_state, _actions);
+			}
+
+		public:
+
+			//constructor for root node
+			MinimaxNode(const State& state, const ParamPackage& params):
+				_state(state),
+				_depth(0),
+				_actions(),
+				_winner(),
+				_params(params)
+			{
+				NodeInit(params);
+			}
+
+			//constructor for child node.
+			MinimaxNode(const State& parent_state, const Action& taken_action, size_t depth, const ParamPackage& params):
+				_state(params.GetNewState(parent_state,taken_action)),
+				_depth(depth),
+				_actions(),
+				_winner(),
+				_params(params)
+			{
+				NodeInit(params);
+			}
+
+			inline bool is_terminal_state() const
+			{
+				return _winnner != _params.no_winner_index;
+			}
+
+			EvalValue GetValue() const
+			{
+				if (_depth == 0 || is_terminal_state())
+				{
+					return _params.EvalForParent(_state, _winner);
+				}
+				GADT_CHECK_WARNING(g_ENABLE_MINIMAX_WARNING, _actions.size() == 0, "MM101:empty action set");
+				Node first_child(_state, _actions[0], _depth - 1, _params);
+				EvalValue best_value = first_child.GetValue();
+				for (size_t i = 1; i < _actions.size(); i++)
+				{
+					Node child(_state, _actions[i], _depth - 1, _params);
+					EvalValue child_value = child.GetValue();
+					if (child_value >= best_value)
+					{
+						best_value = child_value;
+					}
+				}
+				return best_value;
+			}
+		};
+
+		template<typename State, typename Action, typename Result, bool _is_debug>
+		class MinimaxSearch
+		{
+		private:
+
+		public:
+		};
 	}
 }
